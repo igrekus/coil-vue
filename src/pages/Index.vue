4d88e2c079be992e74d0c1d7ba16d115478be360
igrekus <upload234@mail.ru>
2019-09-03 17:21:51 +0300

    WIP: UI stub

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,60 @@
-<template>
-  <q-page class="flex flex-center">
-      <div class="q-p-md">
-          <div class="col-auto">heyyy</div>
-          <div class="col-grow">heyyy</div>
-      </div>
-  </q-page>
+/<template>
+    <div class="q-pa-lg">
+
+        <div class="row row-xl justify-center">
+            <div class="col-3">
+                <q-input
+                    outlined
+                    v-model.number="wireGap"
+                    label="d="
+                    style="max-width: 300px"
+                    type="number"
+                    debounce="500"
+                    suffix="мм"
+                    mask="#.##"
+                    fill-mask="1.00"
+                    reverse-fill-mask
+                    dense
+                    :decimals="2"
+                    :step="0.1"
+                    :rules="[ isValidWireGap ]"
+                />
+                <q-input
+                    outlined
+                    v-model.number="loopCount"
+                    label="n="
+                    style="max-width: 300px"
+                    type="number"
+                    debounce="500"
+                    suffix="шт"
+                    mask="#№"
+                    fill-mask="2"
+                    reverse-fill-mask
+                    dense
+                    :decimals="0"
+                    :step="1"
+                    :rules="[ isValidLoopCount || 'lalka azazaz' ]"
+                />
+                <div class="q-gutter-sm row items-start">
+                    <q-uploader
+                            label="Открыть файл"
+                            auto-upload
+                            url="http://localhost:4444/upload"
+                            @failed="onUploadFailed"
+                            @uploaded="onUploadFinished"
+                    />
+                </div>
+                <br/>
+                <div class="row rox-xl">
+                    <q-btn class="col-auto" color="primary" @click="onCalcClick" :disable="isReadyToCalc">Рассчитать</q-btn>
+                </div>
+            </div>
+            <div class="col-6" style="border: 1px solid green">
+                .col-12 .col-md-auto (Variable width content)
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <style>
@@ -12,6 +62,42 @@
 
 <script>
 export default {
+  data: function () {
+    return {
+      wireGap: 0.5,
+      loopCount: 2,
+      isFileReady: false
+    }
+  },
+  methods: {
+    isValidWireGap (val) {
+      return (val > 0) && (val < 10)
+    },
+    isValidLoopCount (val) {
+      return (val > 0) && (val < 10)
+    },
+    onCalcClick () {
+      if (!this.isValidWireGap(this.wireGap) || !this.isValidLoopCount(this.loopCount)) return
+      console.log('click')
+      this.wireGap = 5
+    },
+    onUploadFailed (info) {
+      if (info.files.length) {
+        this.isFileReady = false
+      }
+    },
+    onUploadFinished (info) {
+      if (info.files.length) {
+        console.log('uplaod success')
+        this.isFileReady = true
+      }
+    }
+  },
+  computed: {
+    isReadyToCalc () {
+      return !(this.isValidWireGap(this.wireGap) && this.isValidLoopCount(this.loopCount) && this.isFileReady)
+    }
+  },
   name: 'PageIndex'
 }
 </script>
